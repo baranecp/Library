3d5f81b880d8ec66483a73da5765b8f7e5e253de
baranecp <baranec.dev@gmail.com>
2025-08-05 13:09:31 +0200

    Refactor to use class

--- index.js
+++ index.js
@@ -2,56 +2,89 @@ const myLibrary = [];
 const showBtn = document.getElementById("show-dialog");
 const dialog = document.getElementById("dialog");
 const closeBtn = document.getElementById("close-dialog");
-const form = dialog.querySelector('form');
+const form = dialog.querySelector("form");
 
-
-function Book(title, author, read, id) {
-    if (!new.target) {
-        throw Error("Use the 'new' operator!");
-    }
+class Book {
+  constructor(title, author, read, id) {
     this.title = title;
     this.author = author;
     this.read = read;
     this.id = id;
-}
+  }
 
-Book.prototype.toggleRead = function () {
-    this.read = this.read === 'Read' ? 'Not read yet' : 'Read';
+  set title(newTitle) {
+    if (typeof newTitle === "string" && newTitle.length > 0) {
+      this._title = newTitle;
+    } else {
+      throw new Error("Title must be a non-empty string!");
+    }
+  }
+
+  set author(newAuthor) {
+    if (typeof newAuthor === "string" && newAuthor.length > 0) {
+      this._author = newAuthor;
+    } else {
+      throw new Error("Author must be a non-empty string!");
+    }
+  }
+
+  set read(newIsRead) {
+    this._isRead = Boolean(newIsRead);
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get read() {
+    return this._isRead;
+  }
+
+  toggleRead() {
+    this.read = !this.read;
+  }
 }
 
 function addBookToLibrary(title, author, read) {
-    const id = crypto.randomUUID();
-    const book = new Book(title, author, read, id);
-    myLibrary.push(book);
+  const id = crypto.randomUUID();
+  const book = new Book(title, author, read, id);
+  myLibrary.push(book);
 }
 
 function displayBooks() {
-    document.querySelector('.books').innerHTML = myLibrary.map(book =>
+  document.querySelector(".books").innerHTML = myLibrary
+    .map(
+      (book) =>
         `
         <div class="book" data-id=${book.id}>
             <h3>${book.title}</h3>
             <p>${book.author}</p>
-            <p class="read-toggle" data-id=${book.id}>${book.read}</p>
+            <p class="read-toggle">${book.read ? "Read" : "Not read yet"}</p>
             <a class="remove">X</a>
         </div>
         `
-    ).join('')
+    )
+    .join("");
 }
 
 showBtn.addEventListener("click", () => dialog.showModal());
 
 closeBtn.addEventListener("click", () => {
-    dialog.close();
-    dialog.querySelector('form').reset();
+  dialog.close();
+  dialog.querySelector("form").reset();
 });
 
 //Handle add book
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const title = form.querySelector('#title').value;
-  const author = form.querySelector('#author').value;
-  const read = form.querySelector('#read').value;
+  const title = form.querySelector("#title").value.trim();
+  const author = form.querySelector("#author").value.trim();
+  const read = form.querySelector("#read").value;
 
   addBookToLibrary(title, author, read);
 
@@ -61,23 +94,23 @@ form.addEventListener('submit', (e) => {
 });
 
 //Handle remove
-document.querySelector('.books').addEventListener('click', (e) => {
-    if(e.target.classList.contains('remove')) {
-        const bookId = e.target.closest('.book').dataset.id;
-        const index = myLibrary.findIndex(book => book.id === bookId);
-        if(index !== -1) {
-            myLibrary.splice(index, 1);
-            displayBooks();
-        }
+document.querySelector(".books").addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove")) {
+    const bookId = e.target.closest(".book").dataset.id;
+    const index = myLibrary.findIndex((book) => book.id === bookId);
+    if (index !== -1) {
+      myLibrary.splice(index, 1);
+      displayBooks();
     }
+  }
 
-    //Handle toggle read
-    if(e.target.classList.contains('read-toggle')) {
-        const bookId = e.target.dataset.id;
-        const book = myLibrary.find(book => book.id === bookId);
-        if(book) {
-            book.toggleRead();
-            displayBooks();
-        }
+  //Handle toggle read
+  if (e.target.classList.contains("read-toggle")) {
+    const bookId = e.target.dataset.id;
+    const book = myLibrary.find((book) => book.id === bookId);
+    if (book) {
+      book.toggleRead();
+      displayBooks();
     }
-})
+  }
+});
